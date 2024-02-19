bd607080740208e5dd7c0e71f6294bd258dddcef
Keith <55594418+keithkelsy@users.noreply.github.com>
2024-02-19 09:20:10 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -12,18 +12,30 @@ def load_input(input_directory):
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
     #
-
+    filenames = glob.glob(input_directory + '/*.*')
+    datafranes = [
+        pd.read_csv(filename, sep=";", names=["text"]) for filename in filenames
+    ]
+    dataframe = pd.concat(dataframes).reset_index(drop=True)
+    return dataframe
+    
 
 def clean_text(dataframe):
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.lower()
+    dataframe['text'] = dataframe['text'].str.replace(",", "").str.replace(".", "")
+    return dataframe
 
 
 def count_words(dataframe):
     """Word count"""
-
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.split()
+    return
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
